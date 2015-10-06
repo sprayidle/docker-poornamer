f9b53ab2dd7f3e3c0cadf16b96c53f3d147d30e7
sprayidle <sprayidle@gmail.com>
2015-10-06 13:41:04 +1300

    fix wget command

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /tmp &&
-wget -o get-pip.py https://bootstrap.pypa.io/get-pip.py &&
+wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py &&
 python get-pip.py
 cd /source && \
 python setup.py develop
