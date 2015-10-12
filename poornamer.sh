198359e4b1b2effa27d917693b97780310dccc6f
sprayidle <sprayidle@gmail.com>
2015-10-12 14:47:21 +1300

    virt

--- poornamer.sh
+++ poornamer.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+export WORKON_HOME=/config/virtualenv
+export PROJECT_HOME=/source
+source /usr/local/bin/virtualenvwrapper.sh
 workon poornamer
 exec /sbin/setuser nobody python /source/poornamer.py -settingspath /config
